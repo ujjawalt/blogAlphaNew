e5fdd14f748a3d1b1d2d0525481aa7a9df351a56
ujjawalt <tanwar896@outlook.com>
2022-05-25 08:37:39 +0530

    Cerated controller and view for Articles

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 4, maximum: 100}
+    validates :description, presence: true, length: { minimum: 4, maximum: 300}
+
+end
--- db/migrate/20220524020937_add_timestamps_to_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddTimestampsToArticles < ActiveRecord::Migration[7.0]
-  def change
-    #This won't work in rails 7 as it add the created_at and updated_at fields even if we don't specify it. rails db"migrate will show error : duplicate column name!!
-    add_column :articles, :created_at, :datetime
-    add_column :articles, :updates_at, :datetime
-  end
-end
