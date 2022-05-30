5d2412583fccc353336484225281601f70888ffd
ujjawalt <tanwar896@outlook.com>
2022-05-30 08:25:22 +0530

    Added new article page,validation error display & set up flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,20 @@ class ArticlesController < ApplicationController
         @articles = Article.all    
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def create 
+        @article = Article.new(params.require(:articles).permit(:title, :description))
+        if
+            @article.save
+            flash[:notice] = "Article was created successfully"
+            redirect_to @article
+            #redirect_to article_path(@article) will do the same action as above code. 
+        else
+            render 'new'
+        end
+    end 
+
 end
